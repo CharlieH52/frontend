86aa29e7cbf14503601e1930909da43677d2731e
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-10-17 14:23:25 -0600

    feat: add game board to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
 import styles from "./page.module.css";
+import OuijaBoard from "./components/OuijaBoard.jsx";
 
 export default function Home() {
 	console.log({ styles });
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.title}>Devathon X</h1>
-			<h2 className={styles.subtitle}>Equipo 3</h2>
-			<h2 className={styles.subtitle}>Ouija Virtual</h2>
+			<OuijaBoard />
 		</div>
 	);
 }
