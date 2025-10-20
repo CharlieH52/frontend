d6dd02a376b517bc1f49b903d5a61203c348da0a
Charlie Chan <carlos.gomez.h52@gmail.com>
2025-10-19 20:19:29 -0600

    feat: add mobile frame for input accesibility

--- src/components/MobileFrame.jsx
+++ src/components/MobileFrame.jsx
@@ -1,15 +1,15 @@
 import styles from "@/styles/MobileFrame.module.css";
+import InputSection from "./InputSection";
 
-export default function InputFrame() {
+export default function MobileFrame() {
     return (
-        <section className={styles.UserInput}>
-            <span className={styles.UserInput__Response}>
-                <p>Respuesta desde el mas alla...</p>
-            </span>
-            <div className={styles.UserInput__Controls}>
-                <input className={styles.UserInput__Input} type="text" placeholder="Try to ask something..." />
-                <button className={styles.UserInput__InputSubmit} type="button">Send</button>
+        <div className={styles.MobileFrame}>
+            <div className={styles.MobileFrame__Tab}>
+                <div className="TabLines__Default TabLines__Default--Horizontal"></div>
+                <div className="TabLines__Default TabLines__Default--Horizontal"></div>
+                <div className="TabLines__Default TabLines__Default--Horizontal"></div>
             </div>
-        </section>
+            <InputSection />
+        </div>
     );
 }
