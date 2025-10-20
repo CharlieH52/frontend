63e92a8d88570578163c276cd0ec282325bdc2a0
Charlie Chan <carlos.gomez.h52@gmail.com>
2025-10-19 20:26:01 -0600

    feat: add web input frame as component

--- src/components/WebFrame.jsx
+++ src/components/WebFrame.jsx
@@ -1,25 +1,21 @@
 import styles from "@/styles/WebFrame.module.css";
+import InputSection from "./InputSection";
+import IconLink from "./IconLink";
 
-export default function SideFrame() {
+export default function WebFrame() {
     return (
-        <aside className={styles.UserInput}>
-            <div>
-                <img src="" alt="" />
+        <aside className={styles.WebFrame}>
+            <div className={styles.WebFrame__Tab}>
+                <div className="TabLines__Default TabLines__Default--Vertical"></div>
+                <div className="TabLines__Default TabLines__Default--Vertical"></div>
+                <div className="TabLines__Default TabLines__Default--Vertical"></div>
             </div>
-                <section>
-                    <span className={styles.UserInput__Response}>
-                        <p>Respuesta desde el mas alla...</p>
-                    </span>
-                    <div className={styles.UserInput__Controls}>
-                        <input className={styles.UserInput__Input} type="text" placeholder="Try to ask something..." />
-                        <button className={styles.UserInput__InputSubmit} type="button">Send</button>
-                    </div>
-                </section>
-            <nav>
-                <a href="">CodexBarba</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
+            <div className={styles.WebFrame__Logo}>
+                <img className={styles.WebFrame__LogoImage} src="" alt="" />
+            </div>
+            <InputSection />
+            <nav className={styles.WebFrame__Layout}>
+                <IconLink img_src="/assets/icons/PEE_logo.png" img_alt="Programación en Español" url="https://programacion-es.dev/" />
             </nav>
         </aside>
     );
