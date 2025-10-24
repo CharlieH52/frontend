66c5ed875e9748ba91f0fbbcf659342339009d67
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2025-10-24 22:05:06 +0200

    style: update eslint and prettier configs
    - Change tabWidth from 2 to 4 in prettier config
    - Add @eslint/js recommended config and reorganize eslint rules
    - Update ignore patterns and add no-unused-vars warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,16 +11,23 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**'],
+  },
+
+  js.configs.recommended,
   ...compat.extends('next/core-web-vitals'),
   ...compat.extends('prettier'),
+
   {
+    files: ['**/*.{js,jsx}'],
     rules: {
-      'react/react-in-jsx-scope': 'off',
+      'react/react-in-jsx-scope': 'off', // No necesario en React 17+
       'react/prop-types': 'off',
       'no-console': 'off',
       'react-hooks/exhaustive-deps': 'warn',
+      'no-unused-vars': 'warn',
     },
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
 ];
 
--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  singleQuote: true,
-  semi: true,
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  jsxBracketSameLine: false,
-  jsxSingleQuote: false,
-  trailingComma: "es5",
-  arrowParens: "always",
-  endOfLine: 'lf'
+    singleQuote: true,
+    semi: true,
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    bracketSameLine: false,
+    jsxSingleQuote: false,
+    trailingComma: 'es5',
+    arrowParens: 'always',
+    endOfLine: 'lf',
 };
