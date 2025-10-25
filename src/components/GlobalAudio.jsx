9e01d09f54d6f6af42895f43c564c4d69086a561
Charlie Chan <carlos.gomez.h52@gmail.com>
2025-10-25 12:29:55 -0600

    fix: audio constant missing during the script execution
    - Audio need the complete call using .current\n- add new global variables for icons\n- em units may cause slight jump, probably the icons are not correctly centered

--- src/components/GlobalAudio.jsx
+++ src/components/GlobalAudio.jsx
@@ -3,28 +3,34 @@ import React, { useState, useRef, useEffect, useCallback } from 'react';
 const spookyMusic = '/music/tilitilibom.mp3';
 import styles from "@/styles/GlobalAudio.module.css";
 
-const VolumeIcon = () => '🔊'; 
-const MuteIcon = () => '🔇';
+// CAREFUL: Dont use em units for these elements, it may cause a slight jump during HOVER
+const VolumeIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" viewBox="0 0 24 24"><path fill="currentColor" d="M9 4H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2m8 0h-2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2"/></svg>; 
+const MuteIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" viewBox="0 0 24 24"><path fill="currentColor" d="M6 4v16a1 1 0 0 0 1.524.852l13-8a1 1 0 0 0 0-1.704l-13-8A1 1 0 0 0 6 4"/></svg>;
 
 const GlobalAudio = () => {
     const [isPlaying, setIsPlaying] = useState(false);
     const audio = useRef(null);
     
     useEffect(() => {
-        if (typeof window !== "undefined") {
-            audio.current = new Audio(spookyMusic); 
-        }
-        audio.loop = true;
-        audio.volume = 0.2;
+        audio.current = new Audio(spookyMusic); 
+
+        audio.current.loop = true;
+        audio.current.volume = 0.2;
+        audio.current.currentTime = 8;
+        
+        return() => {
+            audio.current.pause();
+            audio.current = null;
+        };
     }, []);
 
     const toggleAudio = useCallback(() => {
-        const audio = audioRef.current;
+        if (!audio.current) return;
 
         if (isPlaying) {
-            audio.pause();
+            audio.current.pause();
         } else {
-            audio.play().catch(error => {
+            audio.current.play().catch(error => {
                 console.error("Error al intentar reproducir audio:", error);
             });
         }
@@ -32,13 +38,14 @@ const GlobalAudio = () => {
     }, [isPlaying]);
 
     return (
-        <button 
-            className={styles.AudioControl} 
-            onClick={toggleAudio}
-            title={isPlaying ? "Silenciar" : "Activar Audio"}
-        >
-            {isPlaying ? <VolumeIcon /> : <MuteIcon />}
-        </button>
+        <div className={styles.AudioControl}>
+            <span className={styles.AudioControl__Icon}
+                onClick={toggleAudio}
+                title={isPlaying ? "Silenciar" : "Activar Audio"}
+            >
+                {isPlaying ? <VolumeIcon /> : <MuteIcon />}
+            </span>
+        </div>
     );
 };
 
