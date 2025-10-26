456c5454d0a0552b9f753b7ae108620211ef9fe6
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2025-10-26 18:42:14 +0100

    refactor: restructure app layout and move global styles
    Move layout components and global styles to separate files for better organization. Remove unused font imports and simplify the main layout structure.

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import '@/globals.css';
+
+export const metadata = {
+    title: 'Devathon X - Equipo 3 - Ouija Virtual',
+    description: 'Ouija virtual',
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className="">
+                {/* <WebFrame /> */}
+                {children}
+            </body>
+        </html>
+    );
+}
