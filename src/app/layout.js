5c64ad584b90dfe251fb50809fd329f0c5551381
Charlie Chan <carlos.gomez.h52@gmail.com>
2025-10-26 14:01:14 -0600

    chore: remove old music component from RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MobileFrame from "@/components/MobileFrame.jsx";
-import WebFrame from "@/components/WebFrame.jsx";
-import GlobalAudio from "@/components/GlobalAudio";
 import SmokeEffect from "@/components/SmokeEffect";
 
 const geistSans = Geist({
@@ -24,10 +22,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <GlobalAudio />
         <MobileFrame />
         <SmokeEffect />
-        {/* <WebFrame /> */}
         {children}
       </body>
     </html>
