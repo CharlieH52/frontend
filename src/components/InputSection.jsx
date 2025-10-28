8920ca05021599dbc553c5e4cbb2a556218974aa
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2025-10-28 13:17:55 +0100

    feat(InputSection): add form submission and state management
    Implement message handling using app store state
    Convert input section to controlled form component

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,10 +1,29 @@
-import styles from "@/styles/InputSection.module.css";
+'use client';
+import useAppStore from '@/store/useAppStore';
+import styles from '@/styles/InputSection.module.css';
 
 export default function InputSection() {
+    const setMessage = useAppStore((state) => state.setMessage);
+    const message = useAppStore((state) => state.message);
+    const sendMessage = useAppStore((state) => state.sendMessage);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(message);
+        sendMessage(message);
+    };
     return (
-        <section className={styles.UserInput}>
-            <input className={styles.UserInput__Input} type="text" placeholder="Intenta preguntar algo..." />
-            <button className={styles.UserInput__InputSubmit} type="button">Enviar</button>
-        </section>
+        <form className={styles.UserInput} onSubmit={handleSubmit}>
+            <input
+                className={styles.UserInput__Input}
+                type="text"
+                placeholder="Intenta preguntar algo..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <button className={styles.UserInput__InputSubmit} type="submit">
+                Invocar
+            </button>
+        </form>
     );
 }
