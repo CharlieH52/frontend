cd82c0ba35e65d967bdac40e6e4a09bb9f27a412
Charlie Chan <carlos.gomez.h52@gmail.com>
2025-10-28 18:11:01 -0600

    refactor: update styles to adjust the game with the new layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,8 @@
-import styles from './page.module.css';
-import OuijaBoard from '@/components/OuijaBoard.jsx';
+// import styles from './page.module.css';
 
 export default function Home() {
     return (
-        <div className={styles.container}>
-            <OuijaBoard />
-        </div>
+        <>
+        </>
     );
 }
