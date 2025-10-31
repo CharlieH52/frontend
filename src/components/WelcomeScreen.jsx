961198c15bf374a6a1a69383ae0d2cc963fe8241
Angel Ocampo <angelocampo262@gmail.com>
2025-10-30 21:39:06 -0600

    aceptanding changes

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -5,11 +5,6 @@ import styles from '@/styles/WelcomeScreen.module.css';
 import OuijaBoard from './OuijaBoard';
 import Image from 'next/image';
 import SmokeEffect from './SmokeEffect';
-<<<<<<< HEAD
-import RainEffect from './RainEffect';
-import EvilEyes from './EvilEyes';
-=======
->>>>>>> d28e263 (feat/welcome component)
 
 const DEMON_FACE = "/assets/images/demon-face.png";
 const FISSURE_TEXTURE = "/assets/images/fissure-texture.png"
@@ -32,18 +27,11 @@ export default function WelcomeScreen() {
   return (
     <div className={styles.WelcomeScreenContainer}>
       <OuijaBoard
-<<<<<<< HEAD
-        showAlphabet={false}
-        showNumbers={false}
-        showYesNo={false}
-        showAdios={false}
-=======
         showAlphabetSeparator={false}
         showNumbers={false}
         showYesNo={false}
         showAdios={false}
         showSeparator={false}
->>>>>>> d28e263 (feat/welcome component)
       />
       <Image
         src={FISSURE_TEXTURE}
@@ -52,11 +40,8 @@ export default function WelcomeScreen() {
         width={1920}
         height={1080}
         priority
-<<<<<<< HEAD
-=======
         loading="eager"
         unoptimized
->>>>>>> d28e263 (feat/welcome component)
       />
       <div className={styles.textOverlay}>
         <h1 className={styles.title}>Ouija Virtual</h1>
@@ -81,12 +66,6 @@ export default function WelcomeScreen() {
 
 
       <SmokeEffect isLightEnabled={true} />
-<<<<<<< HEAD
-      {/* <RainEffect />
-      <RainEffect /> */}
-=======
-
->>>>>>> d28e263 (feat/welcome component)
       <Image
         src={DEMON_FACE}
         alt="Demon Face"
@@ -95,10 +74,6 @@ export default function WelcomeScreen() {
         height={1080}
         priority
       />
-<<<<<<< HEAD
-      {/* <EvilEyes flash={flash} /> */}
-=======
->>>>>>> d28e263 (feat/welcome component)
 
     </div>
   );
